bacbaf3831f27698b26d9569ce23f2645b209310
WinKing <igordobricc@gmail.com>
2024-12-07 00:58:20 +0100

    Update

--- 21Pong.py
+++ 21Pong.py
@@ -2,6 +2,7 @@ import time
 from turtle import Screen
 from resources.PongGame.paddle import Paddle
 from resources.PongGame.ball import Ball
+from resources.PongGame.score import Scoreboard
 
 screen = Screen()
 screen.bgcolor("black")
@@ -12,6 +13,7 @@ screen.tracer(0)
 r_paddle = Paddle(350, 0)
 l_paddle = Paddle(-350, 0)
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.move_up, "w")
@@ -22,11 +24,25 @@ game_is_on = True
 
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     ball.move()
 
     # Wall collision
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
+
+    # Paddle collision
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # R Paddle miss detection
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # L Paddle miss detection
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
